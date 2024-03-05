661eaffa005d62971c3bafb5a6f6774d68110938
norwegianwood_97 <jhchoi1997@gmail.com>
2024-03-05 22:06:23 +0900

    sign-up 수정

--- src/routes/sign-up.router.js
+++ src/routes/sign-up.router.js
@@ -11,16 +11,31 @@ const router = express.Router();
 /** 사용자 회원가입 API 트랜잭션 **/
 router.post('/sign-up', async (req, res, next) => {
     try {
-        const { nickname, password, usertype } = await signUpSchema.validateAsync(req.body);
+        const { nickname, password, usertype } = req.body;
+
+        // 데이터 형식 검사
+        if(!nickname || !password){
+            return res.status(400).json({message: '데이터 형식이 올바르지 않습니다.'});
+        }
+        // 닉네임 유효성 검사
+        const nicknameRegex = /^[a-zA-Z0-9]{3,15}$/;
+        if(!nicknameRegex.test(nickname)){
+            return res.status(400).json({message: '닉네임 형식에 일치하지 않습니다.'});
+        }
+
+        // 비밀번호 유효성 검사
+        if(password.length < 8 || password.length > 20 || password.includes(nickname) ){
+            return res.status(400).json({message: '비밀번호 형식에 일치하지 않습니다.'});            
+        }
 
         // 중복된 닉네임 검사
         const isExistNickname = await prisma.users.findFirst({
             where: {nickname},
         });
         if (isExistNickname){
-            throw new Error('DuplicateUserError')
+            return res.status(409).json("message: 중복된 닉네임입니다.");
         }
-        
+
         // 비밀번호 암호화
         const hashedPassword = await bcrypt.hash(password, 10);
 
